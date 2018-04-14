c4ee5551cc145f2911c877d03d43b87189d5ed71
Ricky Chen <rickyc106@gmail.com>
2018-04-14 16:17:53 -0500

    TF publisher works!!

--- igvc_tf2/src/laser_scan_broadcaster.py
+++ igvc_tf2/src/laser_scan_broadcaster.py
@@ -3,6 +3,7 @@ import rospy
 import tf
 import tf2_ros
 import geometry_msgs.msg
+import numpy as np
 from nav_msgs.msg import Odometry
 
 def broadcast_tf(msg):
@@ -13,6 +14,8 @@ def broadcast_tf(msg):
 	t.header.stamp = rospy.Time.now()
 	t.header.frame_id = "chassis"
 	t.child_frame_id = "lane_laser_frame"
+
+	# Update this later depending on position of camera picture IRL
 	t.transform.translation.x = 0.15
 	t.transform.translation.y = 0
 	t.transform.translation.z = 0.1
@@ -22,15 +25,45 @@ def broadcast_tf(msg):
 	# Might use euler angles first then convert to quats
 	###
 
-	t.transform.rotation.x = odom.pose.pose.orientation.x
-	t.transform.rotation.y = odom.pose.pose.orientation.y
-	t.transform.rotation.z = odom.pose.pose.orientation.z
-	t.transform.rotation.w = odom.pose.pose.orientation.w
+	# Retrieve orientation data
+	orientation = odom.pose.pose.orientation
 
+	# Then convert to numpy array
+	quat = np.array([])
+	quat = np.append(quat, orientation.x)
+	quat = np.append(quat, orientation.y)
+	quat = np.append(quat, orientation.z)
+	quat = np.append(quat, orientation.w)
+
+	# Convert to Euler
+	euler = tf.transformations.euler_from_quaternion(quat)
+
+	# Cause tuples can't receive item assignment apparently
+	euler = list(euler)
+
+	# Rotates the exact opposite of RAScal's orientation in order to stay fixed
+	euler[2] = euler[2] * -1
+
+	roll = euler[0]
+	pitch = euler[1]
+	yaw = euler[2]
+
+	# Convert back to quaternion
+	updated_quat = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+
+	# Update transform publisher
+	t.transform.rotation.x = updated_quat[0]
+	t.transform.rotation.y = updated_quat[1]
+	t.transform.rotation.z = updated_quat[2]
+	t.transform.rotation.w = updated_quat[3]
+
+	# Send the 1s and 0s
 	br.sendTransform(t)
 
-	#print('I am working')
-	print(odom.pose.pose.orientation.z)
+	print('I am working')
+
+	#print(updated_quat)
+	#print(odom.pose.pose.orientation.z)
 
 def odom_callback(msg):
 	# Set data to msg
